f661ee0ec98dfbf2802503455794da2b22b7e755
Florent Kermarrec <florent@enjoy-digital.fr>
2019-08-26 11:00:12 +0200

    targets: fix import

--- litex_boards/partner/targets/__init__.py
+++ litex_boards/partner/targets/__init__.py
@@ -1,7 +1,3 @@
-from litex_boards.partner.targets import fomu_evt
-from litex_boards.partner.targets import fomu_hacker
-from litex_boards.partner.targets import fomu_pvt
 from litex_boards.partner.targets import netv2
-from litex_boards.partner.targets import tinyfpga_bx
 from litex_boards.partner.targets import trellisboard
 from litex_boards.partner.targets import ulx3s
