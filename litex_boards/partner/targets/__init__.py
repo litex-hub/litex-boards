346621b9fc71e13af248bbfe59665850d72e7a9c
Rohit Singh <rohit@rohitksingh.in>
2019-09-01 03:02:04 -0500

    partner: add platforms and targets for aller, tagus and nereid boards

--- litex_boards/partner/targets/__init__.py
+++ litex_boards/partner/targets/__init__.py
@@ -1,3 +1,6 @@
+from litex_boards.partner.targets import aller
+from litex_boards.partner.targets import nereid
 from litex_boards.partner.targets import netv2
+from litex_boards.partner.targets import tagus
 from litex_boards.partner.targets import trellisboard
 from litex_boards.partner.targets import ulx3s
