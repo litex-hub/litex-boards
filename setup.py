5e1da47cc4aaa2e04a278b3c58871d09477ba1e2
Florent Kermarrec <florent@enjoy-digital.fr>
2020-04-07 12:01:45 +0200

    setup.py: simplify, switch to Python3.6+ (using python_requires), remove version.
    - Deprecate Python 3.5, switch to Python 3.6+.
    - Remove which was not used or updated. We'll see to get this back when working on releases.

--- setup.py
+++ setup.py
@@ -1,35 +1,19 @@
 #!/usr/bin/env python3
 
-import sys
 from setuptools import setup
 from setuptools import find_packages
-
-
-if sys.version_info[:3] < (3, 5):
-    raise SystemExit("You need Python 3.5+")
 
 
 setup(
     name="litex-boards",
     description="LiteX supported boards",
-    long_description=open("README").read(),
     author="Florent Kermarrec",
     author_email="florent@enjoy-digital.fr",
     url="http://enjoy-digital.fr",
     download_url="https://github.com/litex-hub/litex-boards",
     test_suite="test",
     license="BSD",
-    platforms=["Any"],
-    keywords="HDL ASIC FPGA hardware design",
-    classifiers=[
-        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
-        "Environment :: Console",
-        "Development Status :: Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-    ],
+    python_requires="~=3.6",
     include_package_data=True,
     packages=find_packages(),
 )
