c3ea04b6e9937beea72a8812336c31e79185af6c
Florent Kermarrec <florent@enjoy-digital.fr>
2020-10-12 17:33:40 +0200

    targets/s7/us: update sdram (manual cmd_latency no longer needed).

--- litex_boards/targets/xcu1525.py
+++ litex_boards/targets/xcu1525.py
@@ -76,8 +76,7 @@ class BaseSoC(SoCCore):
                 pads             = platform.request("ddram", ddram_channel),
                 memtype          = "DDR4",
                 sys_clk_freq     = sys_clk_freq,
-                iodelay_clk_freq = 500e6,
-                cmd_latency      = 1)
+                iodelay_clk_freq = 500e6)
             self.add_csr("ddrphy")
             self.add_sdram("sdram",
                 phy                     = self.ddrphy,
