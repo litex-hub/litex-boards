e950a4a588515c69c0eb559f432fa41d35f5eb0c
Florent Kermarrec <florent@enjoy-digital.fr>
2020-10-30 17:12:59 +0100

    targets/kc705: update sata pads.

--- litex_boards/targets/kc705.py
+++ litex_boards/targets/kc705.py
@@ -94,11 +94,11 @@ class BaseSoC(SoCCore):
             # IOs
             _sata_io = [
                 # SFP 2 SATA Adapter / https://shop.trenz-electronic.de/en/TE0424-01-SFP-2-SATA-Adapter
-                ("sfp", 0,
-                    Subsignal("txp", Pins("H2")),
-                    Subsignal("txn", Pins("H1")),
-                    Subsignal("rxp", Pins("G4")),
-                    Subsignal("rxn", Pins("G3")),
+                ("sfp2sata", 0,
+                    Subsignal("tx_p", Pins("H2")),
+                    Subsignal("tx_n", Pins("H1")),
+                    Subsignal("rx_p", Pins("G4")),
+                    Subsignal("rx_n", Pins("G3")),
                 ),
             ]
             platform.add_extension(_sata_io)
@@ -112,7 +112,7 @@ class BaseSoC(SoCCore):
             # PHY
             self.submodules.sata_phy = LiteSATAPHY(platform.device,
                 refclk     = sata_refclk,
-                pads       = platform.request("sfp"),
+                pads       = platform.request("sfp2sata"),
                 gen        = "gen2",
                 clk_freq   = sys_clk_freq,
                 data_width = 16)
