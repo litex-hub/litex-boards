6bf91334364877a40a102635fa1977fd908f8313
Thomas Parks <tom.parks@riverlane.com>
2020-12-27 16:47:22 +0000

    acorn_cle_215: Fix hard-lockup on PCIe bringup.
    The S7 PHY now defaults to the same pcie bus width as the specified
    data width, causing the host to lock up. manually specifing the
    old default (64b) resolves this.

--- litex_boards/targets/acorn_cle_215.py
+++ litex_boards/targets/acorn_cle_215.py
@@ -105,7 +105,7 @@ class BaseSoC(SoCCore):
         # PCIe -------------------------------------------------------------------------------------
         if with_pcie:
             self.submodules.pcie_phy = S7PCIEPHY(platform, platform.request("pcie_x4"),
-                data_width = 128,
+                data_width = 128, pcie_data_width=64,
                 bar0_size  = 0x20000)
             self.add_csr("pcie_phy")
             self.add_pcie(phy=self.pcie_phy, ndmas=1)
