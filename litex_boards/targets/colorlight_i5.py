8692ed462fce18484d0910ade16607ea5e12404f
Kaz Kojima <kkojima@rr.iij4u.or.jp>
2021-02-03 08:17:05 +0900

    targets/colorlight_i5: use .bit stream instead of .svf when loading.

--- litex_boards/targets/colorlight_i5.py
+++ litex_boards/targets/colorlight_i5.py
@@ -228,7 +228,7 @@ def main():
 
     if args.load:
         prog = soc.platform.create_programmer()
-        prog.load_bitstream(os.path.join(builder.gateware_dir, soc.build_name + ".svf"))
+        prog.load_bitstream(os.path.join(builder.gateware_dir, soc.build_name + ".bit"))
 
 if __name__ == "__main__":
     main()
