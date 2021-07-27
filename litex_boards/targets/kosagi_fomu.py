9065cfa75d5fbeebb2e194370ba31cd9284a4b5b
Florent Kermarrec <florent@enjoy-digital.fr>
2021-07-27 19:55:04 +0200

    kosagi_fomu: Switch to LiteSPI.

--- litex_boards/targets/kosagi_fomu.py
+++ litex_boards/targets/kosagi_fomu.py
@@ -99,7 +99,10 @@ class BaseSoC(SoCCore):
         self.bus.add_slave("sram", self.spram.bus, SoCRegion(size=128*kB))
 
         # SPI Flash --------------------------------------------------------------------------------
-        self.add_spi_flash(mode="1x", dummy_cycles=8)
+        from litespi.modules import AT25SF161
+        from litespi.opcodes import SpiNorFlashOpCodes as Codes
+        self.add_spi_flash(mode="4x", module=AT25SF161(Codes.READ_1_1_1), with_master=False)
+        #self.add_spi_flash(mode="1x", dummy_cycles=8) # LiteX SPI Flash Core.
 
         # Add ROM linker region --------------------------------------------------------------------
         self.bus.add_region("rom", SoCRegion(
