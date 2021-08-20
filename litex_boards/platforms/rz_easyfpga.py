9bb27add3e56725d69632e7a556f414f3ed97726
alainlou <az2lou@uwaterloo.ca>
2021-08-20 15:26:21 -0400

    what is this

--- litex_boards/platforms/rz_easyfpga.py
+++ litex_boards/platforms/rz_easyfpga.py
@@ -24,56 +24,7 @@ _io = [
         # Compatible with cheap FT232 based cables (ex: Gaoominy 6Pin Ftdi Ft232Rl Ft232)
         # GND on JP1 Pin 12.
         Subsignal("tx", Pins("114"), IOStandard("3.3-V LVTTL")),
-        Subsignal("rx", Pins("115"),  IOStandard("3.3-V LVTTL"))
-    ),
-
-    # SPI Flash (also used for bistream programming, need to use AS mode and set as "input/output" after configuration)
-    # no idea why they can't support it in JTAG mode
-    ("spiflash4x", 0,
-        Subsignal("cs_n", Pins("8")),
-        Subsignal("clk", Pins("12")),
-        Subsignal("dq", Pins("13 6 132 133")),
-        IOStandard("3.3-V LVTTL")
-    ),
-    ("spiflash", 0,
-        Subsignal("cs_n", Pins("8")),
-        Subsignal("clk", Pins("12")),
-        Subsignal("mosi", Pins("13")),
-        Subsignal("miso", Pins("6")),
-        Subsignal("wp", Pins("132")),
-        Subsignal("hold", Pins("133")),
-        IOStandard("3.3-V LVTTL"),
-    ),
-
-    # set_instance_assignment -name DCLK_PIN OFF -to DCLK
-    # set_instance_assignment -name DATA0_PIN OFF -to D0
-    # set_instance_assignment -name IO_MAXIMUM_TOGGLE_RATE "0MHz" -to *
-
-    # set_global_assignment -name CYCLONEII_RESERVE_NCEO_AFTER_CONFIGURATION "USE AS REGULAR IO"
-    # set_global_assignment -name RESERVE_DATA0_AFTER_CONFIGURATION "USE AS REGULAR IO"
-    # set_global_assignment -name RESERVE_DATA1_AFTER_CONFIGURATION "USE AS REGULAR IO"
-    # set_global_assignment -name RESERVE_FLASH_NCE_AFTER_CONFIGURATION "USE AS REGULAR IO"
-    # set_global_assignment -name RESERVE_DCLK_AFTER_CONFIGURATION "USE AS REGULAR IO"
-    # set_global_assignment -name SDC_FILE rz_easyfpga.sdc
-
-
-    # SDRAM
-    ("sdram_clock", 0, Pins("43"), IOStandard("3.3-V LVTTL")),
-    ("sdram", 0,
-        Subsignal("a", Pins(
-            "76 77 80 83 68 67 66 65",
-            "64 60 75 59")),
-        Subsignal("ba",    Pins("73 74")),
-        Subsignal("cs_n",  Pins("72")),
-        Subsignal("cke",   Pins("58")),
-        Subsignal("ras_n", Pins("71")),
-        Subsignal("cas_n", Pins("70")),
-        Subsignal("we_n",  Pins("69")),
-        Subsignal("dq", Pins(
-            "28 30 31 32 33 34 38 39",
-            "54 53 52 51 50 49 46 44")),
-        Subsignal("dm", Pins("42 55")),
-        IOStandard("3.3-V LVTTL")
+        Subsignal("rx", Pins("115"), IOStandard("3.3-V LVTTL"))
     ),
 ]
 
--- litex_boards/targets/rz_easyfpga.py
+++ litex_boards/targets/rz_easyfpga.py
@@ -20,18 +20,12 @@ from litex.soc.integration.builder import *
 from litex.soc.integration.soc import SoCRegion
 from litex.soc.cores.led import LedChaser
 
-from litedram.modules import HY57V641620FTP
-from litedram.phy import GENSDRPHY
-
-kB = 1024
-
 # CRG ----------------------------------------------------------------------------------------------
 
 class _CRG(Module):
     def __init__(self, platform, sys_clk_freq, sdram_rate="1:1"):
         self.rst = Signal()
         self.clock_domains.cd_sys = ClockDomain()
-        self.clock_domains.cd_sys_ps = ClockDomain(reset_less=True)
 
         # # #
 
@@ -39,31 +33,23 @@ class _CRG(Module):
         clk50 = platform.request("clk50")
 
         # PLL
-        self.submodules.pll = pll = CycloneIVPLL(speedgrade="-8")
+        self.submodules.pll = pll = CycloneIVPLL(speedgrade="-6")
         self.comb += pll.reset.eq(self.rst)
         pll.register_clkin(clk50, 50e6)
         pll.create_clkout(self.cd_sys, sys_clk_freq)
-        pll.create_clkout(self.cd_sys_ps, sys_clk_freq, phase=90)
-
-        # SDRAM clock
-        self.comb += platform.request("sdram_clock").eq(self.cd_sys_ps.clk)
 
 # BaseSoC ------------------------------------------------------------------------------------------
 
 class BaseSoC(SoCCore):
-    mem_map = {**SoCCore.mem_map, **{"spiflash": 0x80000000}}
-    def __init__(self, bios_flash_offset, spi_flash_module="W25Q16JV", sys_clk_freq=int(50e6),
-                 with_led_chaser=True, **kwargs):
+    def __init__(self, sys_clk_freq=int(50e6), with_led_chaser=True, **kwargs):
+
         print(kwargs)
 
         platform = rz_easyfpga.Platform()
 
-        # Use external ROM since too large for Cyclone IV EP4CE6
-        kwargs["integrated_rom_size"]  = 0
-        kwargs["integrated_sram_size"] = 0x800
-
-        # Set CPU variant / reset address
-        kwargs["cpu_reset_address"] = self.mem_map["spiflash"] + bios_flash_offset
+        # Limit internal rom and sram size
+        kwargs["integrated_rom_size"]  = 0x6000
+        kwargs["integrated_sram_size"] = 0x1000
 
         # SoCCore ----------------------------------------------------------------------------------
         SoCCore.__init__(self, platform, sys_clk_freq,
@@ -73,32 +59,6 @@ class BaseSoC(SoCCore):
 
         # CRG --------------------------------------------------------------------------------------
         self.submodules.crg = _CRG(platform, sys_clk_freq)
-
-        # SDR SDRAM --------------------------------------------------------------------------------
-        if not self.integrated_main_ram_size:
-            self.submodules.sdrphy = GENSDRPHY(platform.request("sdram"), sys_clk_freq)
-            self.add_sdram("sdram",
-                phy           = self.sdrphy,
-                module        = HY57V641620FTP(sys_clk_freq, "1:1"), # Hynix
-                l2_cache_size = kwargs.get("l2_size", 0)
-            )
-
-        # SPI Flash --------------------------------------------------------------------------------
-        from litespi.modules import W25Q16JV
-        from litespi.opcodes import SpiNorFlashOpCodes as Codes
-
-        # lambdas for lazy module instantiation.
-        spi_flash_modules = {
-            "W25Q16JV":  lambda: W25Q16JV( Codes.READ_1_1_4),
-        }
-        self.add_spi_flash(mode="4x", module=spi_flash_modules[spi_flash_module](), with_master=False)
-
-        # Add ROM linker region --------------------------------------------------------------------
-        self.bus.add_region("rom", SoCRegion(
-            origin = self.mem_map["spiflash"] + bios_flash_offset,
-            size   = 32*kB,
-            linker = True)
-        )
 
         # Leds -------------------------------------------------------------------------------------
         if with_led_chaser:
@@ -113,15 +73,11 @@ def main():
     parser.add_argument("--build",        action="store_true", help="Build bitstream")
     parser.add_argument("--load",         action="store_true", help="Load bitstream")
     parser.add_argument("--sys-clk-freq", default=50e6,        help="System clock frequency (default: 50MHz)")
-    parser.add_argument("--bios-flash-offset", default=0x20000,     help="BIOS offset in SPI Flash (default: 0x20000)")
     builder_args(parser)
     soc_core_args(parser)
     args = parser.parse_args()
 
-    dfu_flash_offset = 0x40000
-
     soc = BaseSoC(
-        bios_flash_offset = dfu_flash_offset + args.bios_flash_offset,
         sys_clk_freq = int(float(args.sys_clk_freq)),
         **soc_core_argdict(args)
     )
