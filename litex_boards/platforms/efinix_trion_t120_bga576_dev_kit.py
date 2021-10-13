5b4558c9b9c67ec94461f8ed68a6820adc1a8c79
Florent Kermarrec <florent@enjoy-digital.fr>
2021-10-13 16:32:58 +0200

    platforms/efinix_trion_t120_bga576: Add Buttons and Serial (other PMOD USB-UART).

--- litex_boards/platforms/efinix_trion_t120_bga576_dev_kit.py
+++ litex_boards/platforms/efinix_trion_t120_bga576_dev_kit.py
@@ -24,6 +24,19 @@ _io = [
     ("user_led", 5, Pins("W16"),  IOStandard("3.3_V_LVTTL_/_LVCMOS"), Misc("DRIVE_STRENGTH=3")),
     ("user_led", 6, Pins("AD16"), IOStandard("3.3_V_LVTTL_/_LVCMOS"), Misc("DRIVE_STRENGTH=3")),
     ("user_led", 7, Pins("AC16"), IOStandard("3.3_V_LVTTL_/_LVCMOS"), Misc("DRIVE_STRENGTH=3")),
+
+    # Buttons
+    ("user_btn", 0, Pins("AD15"), IOStandard("3.3_V_LVTTL_/_LVCMOS"), Misc("WEAK_PULLUP")),
+    ("user_btn", 1, Pins("AC15"), IOStandard("3.3_V_LVTTL_/_LVCMOS"), Misc("WEAK_PULLUP")),
+    ("user_btn", 2, Pins("W15"),  IOStandard("3.3_V_LVTTL_/_LVCMOS"), Misc("WEAK_PULLUP")),
+    ("user_btn", 3, Pins("V15"),  IOStandard("3.3_V_LVTTL_/_LVCMOS"), Misc("WEAK_PULLUP")),
+
+    # Serial / PMOD USB-UART on PMOD E.
+    ("serial", 0,
+        Subsignal("tx", Pins("B23")),
+        Subsignal("rx", Pins("A20")),
+        IOStandard("3.3_V_LVTTL_/_LVCMOS")
+    ),
 ]
 
 # Platform -----------------------------------------------------------------------------------------
