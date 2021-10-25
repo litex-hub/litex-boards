c7a91f9eab294a05656bdd5f8f00ca98d83ec38b
Florent Kermarrec <florent@enjoy-digital.fr>
2021-10-25 19:33:49 +0200

    efinix: Enable identifier on SoC (issue fixed in LiteX).

--- litex_boards/targets/efinix_trion_t120_bga576_dev_kit.py
+++ litex_boards/targets/efinix_trion_t120_bga576_dev_kit.py
@@ -49,8 +49,8 @@ class BaseSoC(SoCCore):
 
         # SoCCore ----------------------------------------------------------------------------------
         SoCCore.__init__(self, platform, sys_clk_freq,
-            #ident          = "LiteX SoC on Efinix Trion T120 BGA576 Dev Kit", # FIXME: Crash design.
-            #ident_version  = True,
+            ident         = "LiteX SoC on Efinix Trion T120 BGA576 Dev Kit",
+            ident_version = True,
             **kwargs
         )
 
