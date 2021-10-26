91818bc5f06a0e8971779846c7c478b836bd4d09
Florent Kermarrec <florent@enjoy-digital.fr>
2021-10-26 17:01:55 +0200

    targets/gsd_butterstick/BaseSoC: Set default device to 85F (consistency with default arguments).

--- litex_boards/targets/gsd_butterstick.py
+++ litex_boards/targets/gsd_butterstick.py
@@ -84,7 +84,7 @@ class _CRG(Module):
 # BaseSoC ------------------------------------------------------------------------------------------
 
 class BaseSoC(SoCCore):
-    def __init__(self, revision="1.0", device="25F", sys_clk_freq=int(60e6), toolchain="trellis",
+    def __init__(self, revision="1.0", device="85F", sys_clk_freq=int(60e6), toolchain="trellis",
         with_ethernet=False, with_etherbone=False, eth_ip="192.168.1.50", eth_dynamic_ip=False,
         with_spi_flash=False,
         with_led_chaser=True,
