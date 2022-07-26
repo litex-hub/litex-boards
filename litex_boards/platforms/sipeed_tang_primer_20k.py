d49e43801e274c46704ee540c1c724e21f482cfc
Florent Kermarrec <florent@enjoy-digital.fr>
2022-07-26 11:30:09 +0200

    sipeed_tang_primer_20k: Add 204 Pins SODIMM Connector.
    Will allow defining Dock peripherals.

--- litex_boards/platforms/sipeed_tang_primer_20k.py
+++ litex_boards/platforms/sipeed_tang_primer_20k.py
@@ -2,6 +2,7 @@
 # This file is part of LiteX-Boards.
 #
 # Copyright (c) 2022 Icenowy Zheng <icenowy@aosc.io>
+# Copyright (c) 2022 Florent Kermarrec <florent@enjoy-digital.fr>
 # SPDX-License-Identifier: BSD-2-Clause
 
 from migen import *
@@ -50,9 +51,60 @@ _io = [
     ),
 ]
 
-# Connectors ---------------------------------------------------------------------------------------
+# 204 Pins SODIMM Connector ------------------------------------------------------------------------
 
-_connectors = []
+_connectors = [
+    ["CARD1A",
+        "---", # 0
+        #     GND GND  5V  5V  5V  5V GND GND  NC   ( 1-10).
+        " T13 --- --- --- --- --- --- --- --- ---",
+        #      NC GND GND      NC  NC  NC GND GND   (11-20).
+        " M11 --- --- --- T10 --- --- --- --- ---",
+        #  NC 3V3  NC 3V3 GND GND                   (21-30).
+        " --- --- --- --- --- ---  T6 R16  P6 P15",
+        # GND GND                 GND GND           (31-40).
+        " --- ---  T7 P16  R8 N15 --- ---  T8  N16",
+        #                             GND GND       (41-50).
+        "  M6 N14 GND L16  T9 L14  P9 --- --- K15",
+        #             GND GND                 GND   (51-60).
+        " P11 K14 T11 --- --- K16 R11 J15 T12 ---",
+        # GND                 GND                   (61-70).
+        " --- H16 R12 H14 P13 --- R13 G16 T14 H15",
+        # GND GND                                   (71-72).
+        " --- ---",
+    ],
+    ["CARD1B",
+        "---", # 0
+        #                                      NC   (73-82).
+        " M15 L13 M14 K11 F13 K12 G12 K13 T15 ---",
+        #                  NC  NC                   (83-92).
+        " J16 H13 J14 J12 --- --- G14 H12 G15 G11",
+        #  NC  NC                  NC  NC E15  NC  (93-102).
+        " --- --- F14 B10 F16 A13 --- --- --- ---",
+        #      NC  NC  NC      NC      NC  NC  NC  (103-112).
+        " D15 --- --- --- A15 --- B14 --- --- ---",
+        #      NC      NC  NC  NC      NC      NC  (113-122).
+        " A14 --- B13 --- --- --- C12 --- B12 ---",
+        #      NC      NC GND GND                  (123-132).
+        " A12 --- C11 --- --- --- B11 E15 A11 F15",
+        # GND GND          NC GND GND      NC      (133-142).
+        " --- --- C10 C13 --- --- --- D16 --- E14",
+        #     GND GND                 GND GND      (143-152).
+        "  B8 --- ---  C9  C6  A9  A7 --- --- L12",
+        #         GND GBD                 GND GND  (153-162).
+        "  A6 J11 --- ---  C7  E9  D7  E8 --- ---",
+        #     VCC     VCC GND GND     VCC     GND  (163-172).
+        "  T2 ---  T3 --- --- ---  T4 ---  T5 ---",
+        # GND VCC             GND GND              (173-182).
+        " --- ---  N6 F10  N7 --- --- D11  N9 D10",
+        #     GND GND      NC  NC GND GND          (183-192).
+        "  R9 --- --- E10 --- --- --- ---  N8 R7",
+        #         GND GND  NC      NC      NC  NC  (193-202).
+        "  L9  P7 --- --- ---  M6 ---  L8 --- ---",
+        #  NC  NC                                  (203-204).
+        " --- ---",
+    ],
+]
 
 # Platform -----------------------------------------------------------------------------------------
 
