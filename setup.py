f047844f86c0ce00593de19fa42e00d45fb5e383
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2023-09-17 17:50:04 -0400

    Add version number, include readme as long description, add explicit litex dependency, use github action for python install, add dependabot action upgrader, update checkout action

--- setup.py
+++ setup.py
@@ -4,9 +4,16 @@ from setuptools import setup
 from setuptools import find_packages
 
 
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+
 setup(
     name="litex-boards",
+    version="2023.08",
     description="LiteX supported boards",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Florent Kermarrec",
     author_email="florent@enjoy-digital.fr",
     url="http://enjoy-digital.fr",
@@ -14,6 +21,7 @@ setup(
     test_suite="test",
     license="BSD",
     python_requires="~=3.6",
+    install_requires=["litex"],
     include_package_data=True,
     packages=find_packages(exclude=['test*']),
 )
