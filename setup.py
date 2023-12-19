e9dac1e46033b558cbb35903b29c5144e8676aa9
Florent Kermarrec <florent@enjoy-digital.fr>
2023-12-19 10:17:34 +0100

    setup.py: Improve indentation and specify UTF-8 encoding for long_description/README.md.

--- setup.py
+++ setup.py
@@ -4,24 +4,24 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-with open("README.md", "r") as fp:
+with open("README.md", "r", encoding="utf-8") as fp:
     long_description = fp.read()
 
 
 setup(
-    name="litex-boards",
-    version="2023.08",
-    description="LiteX supported boards",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Florent Kermarrec",
-    author_email="florent@enjoy-digital.fr",
-    url="http://enjoy-digital.fr",
-    download_url="https://github.com/litex-hub/litex-boards",
-    test_suite="test",
-    license="BSD",
-    python_requires="~=3.6",
-    install_requires=["litex"],
-    include_package_data=True,
-    packages=find_packages(exclude=['test*']),
+    name                          = "litex-boards",
+    version                       = "2023.08",
+    description                   = "LiteX supported boards",
+    long_description              = long_description,
+    long_description_content_type = "text/markdown",
+    author                        = "Florent Kermarrec",
+    author_email                  = "florent@enjoy-digital.fr",
+    url                           = "http://enjoy-digital.fr",
+    download_url                  = "https://github.com/litex-hub/litex-boards",
+    test_suite                    = "test",
+    license                       = "BSD",
+    python_requires               = "~=3.6",
+    install_requires              = ["litex"],
+    include_package_data          = True,
+    packages                      = find_packages(exclude=['test*']),
 )
