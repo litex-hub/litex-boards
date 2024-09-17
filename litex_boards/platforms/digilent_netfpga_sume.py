17e32da4160c22b8f2d8c8a71cfda79f34e182f8
GustavsC <gustavocerq7@gmail.com>
2024-09-17 11:17:47 -0300

    #2
    Ethernet SFP Support enabled.
    SD Card Support enabled.
    Programmer done.

--- litex_boards/platforms/digilent_netfpga_sume.py
+++ litex_boards/platforms/digilent_netfpga_sume.py
@@ -1,6 +1,7 @@
 #"
 # This file is part of LiteX-Boards.
 #
+# Copyright (c) 2019-2024 Florent Kermarrec <florent@enjoy-digital.fr>
 # Copyright (c) 2024 Gustavo Bastos <gustavocerq7@gmail.com>
 # SPDX-License-Identifier: BSD-2-Clause
 
@@ -80,13 +81,131 @@ _io = [
         Misc("VCCAUX_IO=NORMAL"),
     ),
     
+    ("ddram", 1,
+        Subsignal("a", Pins(
+            "G17 J20 H18 D21 D18 C21 J17 E17",
+            "B21 A19 E20 A17 K19 C20 F17 K17"),
+            IOStandard("SSTL15")),
+        Subsignal("ba",    Pins("F20 D17 B19"), IOStandard("SSTL15")),
+        Subsignal("ras_n", Pins("B17"), IOStandard("SSTL15")),
+        Subsignal("cas_n", Pins("D20"), IOStandard("SSTL15")),
+        Subsignal("we_n",  Pins("H20"), IOStandard("SSTL15")),
+        Subsignal("dm", Pins("M13 J13 G14 A14 B23 D26 A31 F31"), IOStandard("SSTL15")),
+        Subsignal("dq", Pins(
+            "M11 M12 N14 M14 N13 L12 L14 N15",
+            "K15 K14 H14 L16 K13 H13 H15 J15",
+            "E14 F15 F16 E15 G12 F12 E13 F14",
+            "D15 D16 B16 C16 E12 C13 B14 D13",
+            "C24 A25 B26 B27 B22 A22 C23 A24",
+            "D25 C25 E24 E23 D22 D23 E22 F22",
+            "A29 A30 A32 D28 C28 C29 D27 C31",
+            "D30 E30 C30 F30 F27 F26 F29 E29"),
+            IOStandard("SSTL15_T_DCI")),
+        Subsignal("dqs_p", Pins("N16 K12 H16 C15 A26 F25 B28 E27"), IOStandard("DIFF_SSTL15_T_DCI")),
+        Subsignal("dqs_n", Pins("M16 J12 G16 C14 A27 E25 B29 E28"), IOStandard("DIFF_SSTL15_T_DCI")),
+        Subsignal("clk_p", Pins("G19 E19"), IOStandard("DIFF_SSTL15")),
+        Subsignal("clk_n", Pins("F19 E18"), IOStandard("DIFF_SSTL15")),
+        Subsignal("cke",   Pins("M17"), IOStandard("SSTL15")),
+        Subsignal("odt",   Pins("J18"), IOStandard("SSTL15")),
+        Subsignal("cs_n",  Pins("C19"), IOStandard("SSTL15")),
+        Subsignal("reset", Pins("A15"), IOStandard("LVCMOS15")),
+        Misc("SLEW=FAST"),
+        Misc("VCCAUX_IO=NORMAL")
+    ),
+    
+    # SDCard
+    ("spisdcard", 0,
+        Subsignal("rst",  Pins("BB12"), IOStandard("LVCMOS15"), Misc("VCCAUX_IO=NORMAL")),
+        Subsignal("clk",  Pins("AJ25")),
+        Subsignal("mosi", Pins("AJ26")),
+        Subsignal("cs_n", Pins("AL26")),
+        Subsignal("miso", Pins("AY29")),
+        Misc("SLEW=FAST"),
+        IOStandard("LVCMOS18"),
+    ),
+    
+    ("sdcard", 0,
+        Subsignal("rst",  Pins("BB12"), IOStandard("LVCMOS15"), Misc("VCCAUX_IO=NORMAL")),
+        Subsignal("data", Pins("AY29 AM28 AL25 AL26")),
+        Subsignal("cmd",  Pins("AJ26")),
+        Subsignal("clk",  Pins("AJ25")),
+        Subsignal("cd",   Pins("AW35")),
+        Misc("SLEW=FAST"),
+        IOStandard("LVCMOS18"),
+    ),
+    
     # I2C
     ("i2c", 0,
     	Subsignal("scl", Pins("AK24"), IOStandard("LVCMOS18")),
     	Subsignal("sda", Pins("AK25"), IOStandard("LVCMOS18")),
     	Misc("VCCAUX_IO=NORMAL")
     ),
+    
+    # SFP - Ethernet
+    ("sfp_clk", 0,
+    	Subsignal("p", Pins("AW32"), IOStandard("LVDS")), #
+    	Subsignal("n", Pins("AW33"), IOStandard("LVDS"))
+    ),
+    
+    ("sfp", 0,
+    	Subsignal("txp", Pins("A6")),
+    	Subsignal("txn", Pins("A5")),
+    	Subsignal("rxn", Pins("B3")),
+    	Subsignal("rxp", Pins("B4"))
+    ),
+    
+    ("sfp", 1,
+    	Subsignal("txp", Pins("B8")),
+    	Subsignal("txn", Pins("B7")),
+    	Subsignal("rxn", Pins("C1")),
+    	Subsignal("rxp", Pins("C2"))
+    ),
+    
+    
+    ("sfp", 2,
+    	Subsignal("txp", Pins("C6")),
+    	Subsignal("txn", Pins("C5")),
+    	Subsignal("rxn", Pins("D3")),
+    	Subsignal("rxp", Pins("D4"))
+    ),
+    
+    ("sfp", 3,
+    	Subsignal("txp", Pins("D8")),
+    	Subsignal("txn", Pins("D7")),
+    	Subsignal("rxn", Pins("E1")),
+    	Subsignal("rxp", Pins("E2"))
+    ),
 
+    ("sfp_tx_disable_n", 0, Pins("M18"), IOStandard("LVCMOS15")),
+    ("sfp_led", Pins("G13 L15"), IOStandard("LVCMOS15")),
+    ("sfp_mod_detect", Pins("N18"), IOStandard("LVCMOS15")),
+    ("sfp_rs", Pins("N19 P18"), IOStandard("LVCMOS15")),
+    ("sfp_rx_los", Pins("L17"), IOStandard("LVCMOS15")),
+    ("sfp_tx_fault_n", Pins("M19"), IOStandard("LVCMOS15")),
+    
+    ("sfp_tx_disable_n", 1, Pins("B31"), IOStandard("LVCMOS15")),
+    ("sfp_led", 1, Pins("AL22 BA20"), IOStandard("LVCMOS15")),
+    ("sfp_mod_detect", 1, Pins("AL19"), IOStandard("LVCMOS15")),
+    ("sfp_rs", 1, Pins("P20 N20"), IOStandard("LVCMOS15")),
+    ("sfp_rx_los", 1, Pins("L20"), IOStandard("LVCMOS15")),
+    ("sfp_tx_disable", 1, Pins("B31"), IOStandard("LVCMOS15")),
+    ("sfp_tx_fault", 1, Pins("C26"), IOStandard("LVCMOS15")),
+    	
+    ("sfp_tx_disable_n", 2, Pins("J38"), IOStandard("LVCMOS15")),
+    ("sfp_led", 2, Pins("AY18 AY17"), IOStandard("LVCMOS15")),
+    ("sfp_mod_detect", 2, Pins("J37"), IOStandard("LVCMOS15")),
+    ("sfp_rs", 2, Pins("F39 G36"), IOStandard("LVCMOS15")),
+    ("sfp_rx_los", 2, Pins("G37"), IOStandard("LVCMOS15")),
+    ("sfp_tx_disable", 2, Pins("J38"), IOStandard("LVCMOS15")),
+    ("sfp_tx_fault", 2, Pins("E39"), IOStandard("LVCMOS15")),
+    	
+    ("sfp_tx_disable_n", 3, Pins("L21"), IOStandard("LVCMOS15")),
+    ("sfp_led", 3, Pins("P31 K32"), IOStandard("LVCMOS15")),
+    ("sfp_mod_detect", 3, Pins("H36"), IOStandard("LVCMOS15")),
+    ("sfp_rs", 3, Pins("H38 G38"), IOStandard("LVCMOS15")),
+    ("sfp_rx_los", 3, Pins("J36"), IOStandard("LVCMOS15")),
+    ("sfp_tx_disable", 3, Pins("L21"), IOStandard("LVCMOS15")),
+    ("sfp_tx_fault", 3, Pins("J26"), IOStandard("LVCMOS15"))
 ]
 
 # Connectors ---------------------------------------------------------------------------------------
@@ -111,7 +230,7 @@ class Platform(Xilinx7SeriesPlatform):
         self.add_platform_command("set_property INTERNAL_VREF 0.750 [get_iobanks 39]")
 
     def create_programmer(self):
-        return OpenOCD() #not tested
+        return OpenOCD("openocd_xc7_ft2232.cfg", "xc7vx690t.bit")
 
     def do_finalize(self, fragment):
         Xilinx7SeriesPlatform.do_finalize(self, fragment)
--- litex_boards/targets/digilent_netfpga_sume.py
+++ litex_boards/targets/digilent_netfpga_sume.py
@@ -3,6 +3,7 @@
 #
 # This file is part of LiteX-Boards.
 #
+# Copyright (c) 2019-2024 Florent Kermarrec <florent@enjoy-digital.fr>
 # Copyright (c) 2024 Gustavo Bastos <gustavocerq7gmail.com> 
 # SPDX-License-Identifier: BSD-2-Clause
 
@@ -24,7 +25,8 @@ from litedram.phy import s7ddrphy
 
 from litedram.common import PHYPadsReducer
 
-from liteeth.phy.rmii import LiteEthPHYRMII
+from liteeth.phy.s7rgmii import LiteEthPHYRGMII
+from liteeth.phy.v7_1000basex import V7_1000BASEX
 from liteeth.phy import LiteEthPHY
 
 # CRG ----------------------------------------------------------------------------------------------
@@ -35,13 +37,15 @@ class _CRG(LiteXModule):
         self.cd_sys       = ClockDomain()
         self.cd_sys4x     = ClockDomain()
         self.cd_idelay = ClockDomain()
+        self.cd_sfp   = ClockDomain()
    
-        self.pll = pll = S7MMCM(speedgrade = -2)
+        self.pll = pll = S7PLL(speedgrade = -2)
         self.comb += pll.reset.eq(platform.request("cpu_reset_n") | self.rst)
         pll.register_clkin(platform.request("clk200"), 200e6)
         pll.create_clkout(self.cd_sys,       sys_clk_freq)
         pll.create_clkout(self.cd_sys4x,     4*sys_clk_freq)
         pll.create_clkout(self.cd_idelay,    200e6)
+        pll.create_clkout(self.cd_sfp,    200e6)
         
         platform.add_false_path_constraints(self.cd_sys.clk, pll.clkin) # Ignore sys_clk to pll.clkin path created by SoC's rst.
 
@@ -51,6 +55,8 @@ class _CRG(LiteXModule):
 
 class BaseSoC(SoCCore):
     def __init__(self, sys_clk_freq=125e6,
+        with_ethernet          = False,
+        with_etherbone         = False,
         with_led_chaser        = True,
         with_i2c               = False,
         **kwargs):
@@ -74,6 +80,23 @@ class BaseSoC(SoCCore):
                 module        = MT8KTF51264(sys_clk_freq, "1:4"),
                 l2_cache_size = kwargs.get("l2_size", 8192)
             )
+            
+        # Ethernet / Etherbone ---------------------------------------------------------------------
+        if with_ethernet or with_etherbone:
+            self.ethphy = V7_1000BASEX(
+                refclk_or_clk_pads = self.crg.cd_sfp.clk,
+                data_pads          = self.platform.request("sfp"),
+                sys_clk_freq       = sys_clk_freq,
+                with_csr           = True
+            )                    
+            self.comb += self.platform.request("sfp_tx_disable_n").eq(1)
+            platform.add_platform_command("set_property SEVERITY {{Warning}} [get_drc_checks UCIO-1]")
+            platform.add_platform_command("set_property SEVERITY {{Warning}} [get_drc_checks REQP-44]")
+        if with_ethernet:
+                self.add_ethernet(phy=self.ethphy)
+        if with_etherbone:
+                self.add_etherbone(phy=self.ethphy)
+                
         # Leds -------------------------------------------------------------------------------------
         if with_led_chaser:
             self.leds = LedChaser(
@@ -90,15 +113,29 @@ def main():
     from litex.build.parser import LiteXArgumentParser
     parser = LiteXArgumentParser(platform=digilent_netfpga_sume.Platform, description="LiteX SoC on NetFPGA-Sume.")
     parser.add_target_argument("--sys-clk-freq", default=125e6, type=float, help="System clock frequency.")
+    parser.add_target_argument("--with-i2c",             action="store_true",     help="Enable I2C support.")
+    ethopts = parser.target_group.add_mutually_exclusive_group()
+    ethopts.add_argument("--with-ethernet",  action="store_true", help="Enable Ethernet support.")
+    ethopts.add_argument("--with-etherbone", action="store_true", help="Enable Etherbone support.")
     sdopts = parser.target_group.add_mutually_exclusive_group()
-    sdopts.add_argument("--with-i2c",             action="store_true",     help="Enable I2C support.")
+    sdopts.add_argument("--with-spi-sdcard",        action="store_true", help="Enable SPI-mode SDCard support.")
+    sdopts.add_argument("--with-sdcard",            action="store_true", help="Enable SDCard support.")
+    
     args = parser.parse_args()
 
     soc = BaseSoC(
         sys_clk_freq           = args.sys_clk_freq,
+        with_ethernet          = args.with_ethernet,
+        with_etherbone         = args.with_etherbone,
         with_i2c               = args.with_i2c,
         **parser.soc_argdict
     )
+    
+    if args.with_spi_sdcard:
+        soc.add_spi_sdcard()
+    if args.with_sdcard:
+        soc.add_sdcard()
+        
     builder = Builder(soc, **parser.builder_argdict)
     if args.build:
         builder.build(**parser.toolchain_argdict)
