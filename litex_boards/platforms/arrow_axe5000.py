db63218e1339fef70937c085386eebba8746ee30
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2025-09-04 09:40:10 +0200

    Added Arrow AXE5000 target (Altera Agilex 5 A5EC008BM16AE6S)

---
+++ litex_boards/platforms/arrow_axe5000.py
@@ -0,0 +1,147 @@
+#
+# This file is part of LiteX-Boards.
+#
+# Copyright (c) 2025 Gwenhael Goavec-merou<gwenhael.goavec-merou@trabucayre.com>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+from litex.build.altera            import AlteraPlatform
+from litex.build.altera.programmer import USBBlaster
+from litex.build.generic_platform  import Pins, IOStandard, Subsignal, Misc
+
+# IOs ----------------------------------------------------------------------------------------------
+
+_io = [
+
+    # Clk
+    ("clk25",      0, Pins("A7"),    IOStandard("1.3-V LVCMOS")),
+    ("refclk",     0, Pins("AJ28"),  IOStandard("3.3-V LVCMOS")),
+    ("sys_clk100", 0, Pins("BF111"), IOStandard("1.3-V LVCMOS")),
+
+    # Serial
+    ("serial", 0,
+        Subsignal("rx", Pins("AG23")),
+        Subsignal("tx", Pins("AG24")),
+        IOStandard("3.3-V LVCMOS"),
+    ),
+
+    # Leds
+    ("user_led", 0, Pins("AG21"), IOStandard("3.3-V LVCMOS")),
+    ("rgb_led",  0,
+        Subsignal("r", Pins("AH22")),
+        Subsignal("g", Pins("AK21")),
+        Subsignal("b", Pins("AK20")),
+        IOStandard("3.3-V LVCMOS"),
+    ),
+
+    # Switches
+    ("user_sw", 0, Pins("A14"), IOStandard("1.3-V LVCMOS")),
+    ("user_sw", 1, Pins("A13"), IOStandard("1.3-V LVCMOS")),
+
+    # Buttons
+    ("user_btn", 0, Pins("A12"), IOStandard("1.3-V LVCMOS"), Misc("WEAK_PULL_UP_RESISTOR ON")),
+
+    # HyperRAM
+    ("hyperram", 0,
+        Subsignal("dq",    Pins("C3 C2 B4 B6 D3 A4 B3 C6")),
+        Subsignal("rwds",  Pins("A6")),
+        Subsignal("cs_n",  Pins("D8")),
+        Subsignal("rst_n", Pins("F7")),
+        Subsignal("clk",   Pins("D7")),
+        IOStandard("1.3-V LVCMOS")
+    ),
+
+    # VADJ selector between 1.2V(low) and 1.3V(high)
+    ("vsel_1v3", 0, Pins("AJ24"), IOStandard("3.3-V LVCMOS")),
+
+    # Accelerometer (LIS3DH)
+    ("accel_int", 0, Pins("AK24 AJ25"), IOStandard("3.3-V LVCMOS")),
+    ("accel_i2c", 0,
+        Subsignal("sda", Pins("AK26")),
+        Subsignal("scl", Pins("AH25")),
+        IOStandard("3.3-V LVCMOS"),
+    ),
+]
+
+# Connectors ---------------------------------------------------------------------------------------
+
+_connectors = [
+    # Arduino MKR Header (J1/J2)
+    ("arduino_io", {
+        # J1
+        "AREF" : "AF22", #  1
+        "AIN0" : "AF23", #  2
+        "AIN1" : "AF24", #  3
+        "AIN2" : "AG26", #  4
+        "AIN3" : "AH28", #  5
+        "AIN4" : "AH27", #  6
+        "AIN5" : "AF27", #  7
+        "AIN6" : "AF26", #  8
+        "D0"   : "AE25", #  9
+        "D1"   : "AF21", # 10
+        "D2"   : "AH20", # 11
+        "D3"   : "AG19", # 12
+        "D4"   : "AF19", # 13
+        "D5"   : "AG20", # 14
+
+        # J2
+        "D6"   : "AK19", #  1
+        "D7"   : "AJ19", #  2
+        "D8"   : "AH21", #  3
+        "D9"   : "AH18", #  4
+        "D10"  : "AJ20", #  5
+        "D11"  : "AJ22", #  6 / Also Connected to AK25
+        "D12"  : "AK22", #  7 / Also Connected to AK27
+        "D13"  : "AH23", #  8
+        "D14"  : "AJ23", #  9
+        "RST"  : "---",  # 10
+        "GND"  : "---",  # 11
+        "3_3V" : "---",  # 12
+        "VIN"  : "---",  # 13
+        "5V"   : "---",  # 14
+    }),
+
+    # CRUVI header
+    ["J3",
+        # B2_P/B2_N (27/29) are assigned to 2 sets of pins on the FPGA
+        # due to MIPI Hard IP pin placement:
+        # B2_P: U5/T4
+        # B2_N: AC6/AC5
+        # -----------------------------------------------------------
+        " ----", # 0
+        #   NC           3.3V                     3.3V        ( 1-10).
+        " ----   N7 AH26 ---- AJ29   N6 AJ27  AF1 ---- AE10",
+        #       GND  GND                      GND  GND        (11-20).
+        " AJ28 ---- ----   N2   V6   N1   W5 ---- ----   R2",
+        #                 GND  GND        T4       AC5  GND   (21-30).
+        "  AC7   T1  AD7 ---- ----   T2   U5   U1  AC6 ----",
+        #  GND                     VADJ  GND                  (31-40).
+        " ----   V1   V3   V2   W3 ---- ----   T3   U4   R4",
+        #  GND  GND  GND                      GND  GND   NC   (41-50).
+        "   U3 ---- ----   Y1   P5   W2   P4 ---- ---- ----",
+        #   NC   NC   NC  GND   NC   NC   NC   NC   NC   5V   (51-60).
+       " ---- ---- ---- ---- ---- ---- ---- ---- ---- ----",
+    ],
+]
+
+# Platform -----------------------------------------------------------------------------------------
+
+class Platform(AlteraPlatform):
+    default_clk_name   = "clk25"
+    default_clk_period = 1e9/25e6
+
+    def __init__(self, toolchain="quartus"):
+        AlteraPlatform.__init__(self, "A5EC008BM16AE6S", _io, _connectors, toolchain=toolchain)
+        self.create_rbf = True
+        self.create_svf = False # Not supported for Agilex5 family.
+        self.add_platform_command("set_global_assignment -name ENABLE_INTERMEDIATE_SNAPSHOTS \"ON\"")
+
+        # Calculates clock uncertainties
+        self.toolchain.additional_sdc_commands.append("derive_clock_uncertainty")
+
+    def create_programmer(self):
+        return USBBlaster(cable_name="USB Blaster III")
+
+    def do_finalize(self, fragment):
+        AlteraPlatform.do_finalize(self, fragment)
+        self.add_period_constraint(self.lookup_request("clk25", loose=True), 1e9/25e6)
---
+++ litex_boards/targets/arrow_axe5000.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+
+#
+# This file is part of LiteX-Boards.
+#
+# Copyright (c) 2025 Gwenhael Goavec-merou<gwenhael.goavec-merou@trabucayre.com>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+from migen import *
+
+from litex.gen import *
+
+from litex_boards.platforms import arrow_axe5000
+
+from litex.soc.interconnect import wishbone
+
+from litex.soc.integration.soc      import *
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.builder  import *
+
+from litex.soc.cores.clock    import Agilex5PLL
+from litex.soc.cores.hyperbus import HyperRAM
+from litex.soc.cores.led      import LedChaser
+
+
+# CRG ----------------------------------------------------------------------------------------------
+
+class _CRG(LiteXModule):
+    def __init__(self, platform, sys_clk_freq):
+        self.rst      = Signal()
+        self.cd_sys   = ClockDomain()
+        self.cd_sys2x = ClockDomain()
+
+        # # #
+
+        # Clk / Rst
+        clk25 = platform.request("clk25")
+        rst_n = platform.request("user_btn", 0)
+
+        # Power on reset
+        ninit_done = Signal()
+        self.specials += Instance("altera_agilex_config_reset_release_endpoint", o_conf_reset = ninit_done)
+
+        # PLL
+        self.pll = pll = Agilex5PLL(speedgrade="-6S")
+        self.comb += pll.reset.eq(ninit_done | ~rst_n)
+        pll.register_clkin(clk25, 25e6)
+        pll.create_clkout(self.cd_sys, sys_clk_freq)
+
+        # HyperRAM
+        pll.create_clkout(self.cd_sys2x, 2*sys_clk_freq)
+
+# BaseSoC ------------------------------------------------------------------------------------------
+
+class BaseSoC(SoCCore):
+    def __init__(self, sys_clk_freq=100e6,
+        with_l2_cache   = False,
+        with_led_chaser = True,
+        **kwargs):
+        platform = arrow_axe5000.Platform()
+
+        # Select 1.3V, for HSIO on CRUVI-HS
+        self.comb += platform.request("vsel_1v3").eq(0b1)
+
+        # CRG --------------------------------------------------------------------------------------
+        self.crg = _CRG(platform, sys_clk_freq)
+
+        # SoCCore ----------------------------------------------------------------------------------
+        SoCCore.__init__(self, platform, sys_clk_freq, ident="LiteX SoC on Arrow AXE5000", **kwargs)
+
+        # HyperRAM ---------------------------------------------------------------------------------
+        if not self.integrated_main_ram_size:
+            # HyperRAM Parameters.
+            hyperram_size       = 16 * MEGABYTE
+            hyperram_cache_size = 16 * KILOBYTE
+
+            # HyperRAM Bus/Slave Interface.
+            hyperram_bus = wishbone.Interface(data_width=32, address_width=32, addressing="word")
+            self.bus.add_slave(
+                name   = "main_ram",
+                slave  = hyperram_bus,
+                region = SoCRegion(origin=self.mem_map["main_ram"], size=hyperram_size, mode="rwx")
+            )
+
+            # HyperRAM L2 Cache.
+            if with_l2_cache:
+                hyperram_cache = wishbone.Cache(
+                    cachesize = hyperram_cache_size // 4,
+                    master    = hyperram_bus,
+                    slave     = wishbone.Interface(data_width=32, address_width=32, addressing="word")
+                )
+                self.hyperram_cache = FullMemoryWE()(hyperram_cache)
+                self.add_config("L2_SIZE", hyperram_cache_size)
+
+            # HyperRAM Core.
+            self.hyperram = HyperRAM(
+                pads         = platform.request("hyperram"),
+                latency      = 7,
+                latency_mode = "variable",
+                sys_clk_freq = sys_clk_freq,
+                clk_ratio    = "2:1", # Not working with 4:1
+            )
+
+            if with_l2_cache:
+                self.comb += self.hyperram_cache.slave.connect(self.hyperram.bus)
+            else:
+                self.comb += hyperram_bus.connect(self.hyperram.bus)
+
+        # Leds -------------------------------------------------------------------------------------
+        if with_led_chaser:
+            led_pads     = platform.request_all("user_led")
+            rgb_led_pads = platform.request("rgb_led", 0)
+            self.comb += [getattr(rgb_led_pads, n).eq(1) for n in "gb"] # Disable Green/Blue Leds.
+            self.leds = LedChaser(
+                pads         = Cat(led_pads, rgb_led_pads.r),
+                sys_clk_freq = sys_clk_freq)
+
+# Build --------------------------------------------------------------------------------------------
+
+def main():
+    from litex.build.parser import LiteXArgumentParser
+    parser = LiteXArgumentParser(platform=arrow_axe5000.Platform, description="LiteX SoC on Arrow AXE5000.")
+    parser.add_target_argument("--sys-clk-freq",  default=100e6, type=float, help="System clock frequency.")
+    parser.add_target_argument("--with-l2-cache", action="store_true",       help="Enable Main RAM L2 cache.")
+
+    # Overrides defaults synth/conv tools.
+    parser.set_defaults(synth_tool = "quartus_syn")
+    parser.set_defaults(conv_tool  = "quartus_pfg")
+
+    # soc.json default path
+    args = parser.parse_args()
+
+    soc = BaseSoC(
+        sys_clk_freq  = args.sys_clk_freq,
+        with_l2_cache = args.with_l2_cache,
+        **parser.soc_argdict
+    )
+
+    builder = Builder(soc, **parser.builder_argdict)
+    if args.build:
+        builder.build(**parser.toolchain_argdict)
+
+    if args.load:
+        prog = soc.platform.create_programmer()
+        prog.load_bitstream(builder.get_bitstream_filename(mode="sram"))
+
+if __name__ == "__main__":
+    main()
