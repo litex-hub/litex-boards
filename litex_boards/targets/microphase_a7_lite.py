b54e1cf9fe74b4ba07b2cc2a44b8e17b977205f5
Matt Reverzani <matt.reverzani@education.wa.edu.au>
2026-02-11 13:47:22 +0800

    add ethernet reset for warm boots

--- litex_boards/targets/microphase_a7_lite.py
+++ litex_boards/targets/microphase_a7_lite.py
@@ -113,6 +113,9 @@ class BaseSoC(SoCCore):
                 rx_delay   = 2e-9,
                 iodelay_clk_freq = 200e6)
             self.add_ethernet(phy=self.ethphy)
+            eth_rst_n = platform.request("eth_rst_n", loose=True)
+            if eth_rst_n is not None:
+                self.comb += eth_rst_n.eq(~self.crg.cd_sys.rst)
 
         # SPI Flash --------------------------------------------------------------------------------
         if with_spi_flash:
