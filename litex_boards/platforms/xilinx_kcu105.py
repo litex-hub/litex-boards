9a13fa633b2e7a678178cf155c08ce67ce06d1ad
Florent Kermarrec <florent@enjoy-digital.fr>
2026-05-24 22:48:38 +0000

    platforms: expose KCU105 SD card detect
    Add the documented SDIO_CD_FPGA signal to the micro-SD resource while preserving the existing CMD/CLK/DAT0..DAT3 mapping.
    
    Sources:
    
    - https://www.xilinx.com/support/documents/boards_and_kits/kcu105/ug917-kcu105-eval-bd.pdf
    
    - https://docs.amd.com/r/en-US/ug917-kcu105-eval-bd

--- litex_boards/platforms/xilinx_kcu105.py
+++ litex_boards/platforms/xilinx_kcu105.py
@@ -97,9 +97,10 @@ _io = [
         IOStandard("LVCMOS18")
     ),
     ("sdcard", 0,
-        Subsignal("clk", Pins("AL10")),
-        Subsignal("cmd", Pins("AD9"), Misc("PULLUP True")),
+        Subsignal("clk",  Pins("AL10")),
+        Subsignal("cmd",  Pins("AD9"), Misc("PULLUP True")),
         Subsignal("data", Pins("AP9 AN9 AH9 AH8"), Misc("PULLUP True")),
+        Subsignal("cd",   Pins("AM10"), Misc("PULLUP True")),
         Misc("SLEW=FAST"),
         IOStandard("LVCMOS18")
     ),
