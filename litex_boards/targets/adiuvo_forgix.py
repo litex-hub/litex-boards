56e2cda74cdc7a78b2129f07a25f36c65682beca
Florent Kermarrec <florent@enjoy-digital.fr>
2026-06-25 10:25:03 +0200

    targets: Add Forgix demo LED core
    Add an optional --with-demo-leds target flag for Adiuvo Forgix. The core exposes CSR-controlled RGB mode, mask, speed and counter registers so the RP2350 SPIBone demo can start FPGA-side LED animations with a small command sequence while preserving the existing LedChaser default.
    
    Tests: python3 -m py_compile litex_boards/targets/adiuvo_forgix.py; python3 -m litex_boards.targets.adiuvo_forgix --build --with-spibone --with-demo-leds --output-dir build/adiuvo_forgix_demo --no-compile

--- litex_boards/targets/adiuvo_forgix.py
+++ litex_boards/targets/adiuvo_forgix.py
@@ -14,6 +14,7 @@ from litex_boards.platforms import adiuvo_forgix
 
 from litex.soc.integration.soc import *
 from litex.soc.integration.builder import *
+from litex.soc.interconnect.csr import *
 from litex.soc.cores.led import LedChaser
 from litex.soc.cores.spi.spi_bone import SPIBone
 
@@ -35,10 +36,71 @@ class _CRG(LiteXModule):
             self.cd_sys.rst.eq(self.rst),
         ]
 
+# DemoLEDs -----------------------------------------------------------------------------------------
+
+class DemoLEDs(LiteXModule):
+    def __init__(self, pads, sys_clk_freq, polarity=1):
+        n = len(pads)
+        self._mode = CSRStorage(2, reset=1, description="LED demo mode: 0 fixed, 1 breathe, 2 chase, 3 rainbow.")
+        self._rgb = CSRStorage(n, reset=2**n - 1, description="RGB LED mask/control value.")
+        self._speed = CSRStorage(8, reset=8, description="Animation speed; larger values slow the demo down.")
+        self._counter = CSRStatus(32, description="Free-running demo counter.")
+
+        # # #
+
+        counter     = Signal(32)
+        prescaler   = Signal(21)
+        step        = Signal(8)
+        pwm_counter = Signal(8)
+        period      = Signal(21)
+        breath      = Signal(8)
+        leds        = Signal(n)
+
+        chase_patterns = Array([1, 2, 4, 2])
+        color_patterns = Array([1, 2, 4, 3, 6, 5, 7, 0])
+        chase          = Signal(n)
+        color          = Signal(n)
+
+        self.comb += [
+            self._counter.status.eq(counter),
+            period.eq((self._speed.storage + 1) << 12),
+            chase.eq(chase_patterns[step[6:8]]),
+            color.eq(color_patterns[step[5:8]]),
+        ]
+
+        self.sync += [
+            counter.eq(counter + 1),
+            pwm_counter.eq(pwm_counter + 1),
+            If(prescaler >= period,
+                prescaler.eq(0),
+                step.eq(step + 1)
+            ).Else(
+                prescaler.eq(prescaler + 1)
+            )
+        ]
+
+        self.comb += [
+            If(step[7],
+                breath.eq(~step)
+            ).Else(
+                breath.eq(step)
+            ),
+            If(self._mode.storage == 0,
+                leds.eq(self._rgb.storage)
+            ).Elif(self._mode.storage == 1,
+                leds.eq(Mux(pwm_counter < breath, self._rgb.storage, 0))
+            ).Elif(self._mode.storage == 2,
+                leds.eq(chase & self._rgb.storage)
+            ).Else(
+                leds.eq(Mux(pwm_counter < breath, color, 0))
+            ),
+            pads.eq(leds ^ (polarity*(2**n - 1))),
+        ]
+
 # BaseSoC ------------------------------------------------------------------------------------------
 
 class BaseSoC(SoCMini):
-    def __init__(self, sys_clk_freq=32e6, with_spibone=False, with_led_chaser=True, **kwargs):
+    def __init__(self, sys_clk_freq=32e6, with_spibone=False, with_led_chaser=True, with_demo_leds=False, **kwargs):
         platform = adiuvo_forgix.Platform()
 
         # CRG --------------------------------------------------------------------------------------
@@ -61,7 +123,9 @@ class BaseSoC(SoCMini):
             self.bus.add_master(name="spibone", master=self.spibone.bus)
 
         # Leds -------------------------------------------------------------------------------------
-        if with_led_chaser:
+        if with_demo_leds:
+            self.demo_leds = DemoLEDs(platform.request_all("user_led_n"), sys_clk_freq=sys_clk_freq, polarity=1)
+        elif with_led_chaser:
             self.leds = LedChaser(
                 pads         = platform.request_all("user_led_n"),
                 sys_clk_freq = sys_clk_freq,
@@ -74,6 +138,7 @@ def main():
     parser = LiteXArgumentParser(platform=adiuvo_forgix.Platform, description="LiteX SoC on Adiuvo Forgix.")
     parser.add_target_argument("--sys-clk-freq", default=32e6, type=float, help="System clock frequency.")
     parser.add_target_argument("--with-spibone", action="store_true", help="Add SPIBone on the RP2350 SPI pins.")
+    parser.add_target_argument("--with-demo-leds", action="store_true", help="Add the CSR-controlled RGB LED demo core.")
     parser.set_defaults(
         cpu_type             = "None",
         integrated_sram_size = 0,
@@ -86,6 +151,7 @@ def main():
     soc = BaseSoC(
         sys_clk_freq = args.sys_clk_freq,
         with_spibone = args.with_spibone,
+        with_demo_leds = args.with_demo_leds,
         **parser.soc_argdict)
     builder = Builder(soc, **parser.builder_argdict)
     if args.build:
