5be8c29f5273b05fd59f35abcd85a2c771a6c7d3
Florent Kermarrec <florent@enjoy-digital.fr>
2026-09-16 16:05:07 +0200

    targets/sipeed_tang_mega_60k: add OV5640 camera control option

--- litex_boards/targets/sipeed_tang_mega_60k.py
+++ litex_boards/targets/sipeed_tang_mega_60k.py
@@ -20,6 +20,7 @@ from litex.soc.integration.builder import *
 from litex.soc.cores.led import LedChaser, WS2812
 from litex.soc.cores.gpio import GPIOIn
 from litex.soc.cores.video import VideoGowinHDMIPHY
+from litex.soc.cores.ov5640 import OV5640Camera
 
 from litedram.modules import AS4C32M16, H5TQ4G63EFR, W9825G6KH6
 from litedram.phy import GENSDRPHY, HalfRateGENSDRPHY
@@ -135,6 +136,7 @@ class BaseSoC(SoCCore):
         sdram_model       = "sipeed",
         sdram_rate        = "1:1",
         with_hdmi         = False,
+        with_camera       = False,
         with_spi_sdcard   = False,
         with_led_chaser   = True,
         with_buttons      = True,
@@ -200,6 +202,10 @@ class BaseSoC(SoCCore):
             self.videophy = VideoGowinHDMIPHY(platform.request("hdmi_out"), clock_domain="hdmi")
             self.add_video_terminal(phy=self.videophy, timings="640x480@60Hz", clock_domain="hdmi")
 
+        # Camera ----------------------------------------------------------------------------------
+        if with_camera:
+            self.camera = OV5640Camera(platform.request("cmos"), sys_clk_freq)
+
         # SD Card ----------------------------------------------------------------------------------
         if with_spi_sdcard:
             platform.add_extension([
@@ -245,6 +251,7 @@ def main():
     parser.add_target_argument("--sdram-model",   default="sipeed", choices=["sipeed", "mister"], help="SDRAM module model.")
     parser.add_target_argument("--sdram-rate",    default="1:1", choices=["1:1", "1:2"],          help="SDRAM clock ratio.")
     parser.add_target_argument("--with-hdmi",     action="store_true",      help="Enable HDMI Video Terminal.")
+    parser.add_target_argument("--with-camera",   action="store_true",      help="Enable OV5640 Camera control.")
     parser.add_target_argument("--with-spi-sdcard", action="store_true",    help="Enable SPI-mode SDCard support.")
     parser.add_target_argument("--with-ws2812",   action="store_true",      help="Enable WS2812 LED.")
     parser.add_target_argument("--without-ddr3",  action="store_true",      help="Disable DDR3 SDRAM.")
@@ -261,6 +268,7 @@ def main():
         sdram_model      = args.sdram_model,
         sdram_rate       = args.sdram_rate,
         with_hdmi        = args.with_hdmi,
+        with_camera      = args.with_camera,
         with_spi_sdcard  = args.with_spi_sdcard,
         with_led_chaser  = not args.without_leds,
         with_buttons     = not args.without_buttons,
