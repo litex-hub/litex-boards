141de120983a247abe3867ab7ac8671a3c2840a3
Florent Kermarrec <florent@enjoy-digital.fr>
2026-09-17 16:35:21 +0200

    targets/digilent_nexys_video: add button/switch GPIO options.

--- litex_boards/targets/digilent_nexys_video.py
+++ litex_boards/targets/digilent_nexys_video.py
@@ -18,6 +18,7 @@ from litex.soc.integration.soc import *
 from litex.soc.integration.builder import *
 from litex.soc.cores.video import VideoS7HDMIPHY
 from litex.soc.cores.led import LedChaser
+from litex.soc.cores.gpio import GPIOIn
 
 from litedram.modules import MT41K256M16
 from litedram.phy import s7ddrphy
@@ -81,6 +82,8 @@ class BaseSoC(SoCCore):
         vadj                   = "1.2V",
         with_video_terminal    = False,
         with_video_framebuffer = False,
+        with_buttons           = False,
+        with_switches          = False,
         **kwargs):
         platform = digilent_nexys_video.Platform(toolchain=toolchain)
 
@@ -188,6 +191,12 @@ class BaseSoC(SoCCore):
                 pads         = platform.request_all("user_led"),
                 sys_clk_freq = sys_clk_freq)
 
+        # Buttons / Switches -----------------------------------------------------------------------
+        if with_buttons:
+            self.buttons = GPIOIn(Cat(platform.request_all("user_btn")))
+        if with_switches:
+            self.switches = GPIOIn(Cat(platform.request_all("user_sw")))
+
         # VADJ -------------------------------------------------------------------------------------
         vadj_map = {"1.2V": 0b00, "1.8V": 0b01, "2.5V": 0b10, "3.3V": 0b11}
         platform.request_all("vadj").eq(vadj_map[vadj])
@@ -212,6 +221,8 @@ def main():
     viopts = parser.target_group.add_mutually_exclusive_group()
     viopts.add_argument("--with-video-terminal",    action="store_true", help="Enable Video Terminal (HDMI).")
     viopts.add_argument("--with-video-framebuffer", action="store_true", help="Enable Video Framebuffer (HDMI).")
+    parser.add_target_argument("--with-buttons",  action="store_true", help="Enable Buttons.")
+    parser.add_target_argument("--with-switches", action="store_true", help="Enable Switches.")
     args = parser.parse_args()
 
     soc = BaseSoC(
@@ -227,6 +238,8 @@ def main():
         vadj                   = args.vadj,
         with_video_terminal    = args.with_video_terminal,
         with_video_framebuffer = args.with_video_framebuffer,
+        with_buttons           = args.with_buttons,
+        with_switches          = args.with_switches,
         **parser.soc_argdict
     )
     if args.with_spi_sdcard:
